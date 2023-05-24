dfb51601564327db16c02959cc0fc111866ebfef
bs0165 <g.criado@alumnos.upm.es>
2023-05-24 02:19:50 +0200

    Patrón Singleton

--- src/main/java/model/Input.java
+++ src/main/java/model/Input.java
@@ -1,6 +1,6 @@
 package model;
 
-import java.util.Scanner;
+import java.util.*;
 
 /**
  * Esta calse nos servirá para escanear lo que le usuario meta por pantalla.
@@ -26,11 +26,20 @@ public class Input {
     }
 
     // METODOS
+    /**
+     * Escanea un String.
+     * @return Devuelve el String escaneado
+     */
     public String scanString(){
         return this.scanner.nextLine();
     }
 
-    public int scanInt() {
+   /**
+    * Escanea un valor de tipo int
+    * @return Devuelve el valor en int.
+    * @throws InputMismatchException En caso que se introdujese algo que no fuera tipo int saltará esta excepción.
+    */
+    public int scanInt() throws InputMismatchException {
         return this.scanner.nextInt();
     }
 }
